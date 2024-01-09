232277b936b94dc350f48b5bb3cade1bcee806a2
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-01-09 23:06:31 +0200

    feat: create Nav component and style it

---
+++ src/app/(components)/Nav.jsx
@@ -0,0 +1,33 @@
+import { IoIosSearch } from "react-icons/io"
+import { IoMoonOutlin, IoSunnyOutline } from "react-icons/io5"
+
+import Image from "next/image"
+
+function Nav() {
+  return (
+    <nav className='flex h-12 px-4 justify-between items-center w-full lg:h-24 lg:px-20'>
+      <Image
+        src='/nowStreaming_icon.png'
+        width={200}
+        height={50}
+        alt='now streaming website'
+        quality={100}
+      ></Image>
+      <ul className='hidden lg:flex justify-between text-2xl w-1/3 cursor-pointer'>
+        <li>Movies</li>
+        <li>TV Shows</li>
+        <li>On Air</li>
+      </ul>
+      <div className='text-2xl w-24 flex justify-between'>
+        <button>
+          <IoIosSearch />
+        </button>
+        <button>
+          <IoSunnyOutline />
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
