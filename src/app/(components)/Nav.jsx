3faaa34c0d5dd8ab10e3379bf14db1baa92d0c22
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-01-15 22:53:29 +0200

    docs: add new Home element in Nav and change the li tags with Links

--- src/app/(components)/Nav.jsx
+++ src/app/(components)/Nav.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { IoIosSearch } from "react-icons/io"
 import { IoMoonOutlin, IoSunnyOutline } from "react-icons/io5"
 
@@ -5,7 +6,7 @@ import Image from "next/image"
 
 function Nav() {
   return (
-    <nav className='flex h-12 justify-between items-center w-full lg:h-24 '>
+    <nav className='flex h-12 justify-between items-center w-full lg:h-24 mb-4'>
       <Image
         src='/nowStreaming_icon.png'
         width={200}
@@ -14,9 +15,18 @@ function Nav() {
         quality={100}
       ></Image>
       <ul className='hidden lg:flex justify-between text-2xl w-1/3 cursor-pointer'>
-        <li>Movies</li>
-        <li>TV Shows</li>
-        <li>On Air</li>
+        <Link href={"/"}>
+          <h2>Home</h2>
+        </Link>
+        <Link href={"/"}>
+          <h2>Movies</h2>
+        </Link>
+        <Link href={"/"}>
+          <h2>TV Shows</h2>
+        </Link>
+        <Link href={"/"}>
+          <h2>On Air</h2>
+        </Link>
       </ul>
       <div className='text-2xl w-24 flex justify-between'>
         <button>
