08282df46e03dbc388d8b45a8572a51f8f6b7961
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-01-16 01:17:11 +0200

    fix: remove h2 tags from inside of Link tags in Nav component

--- src/app/(components)/Nav.jsx
+++ src/app/(components)/Nav.jsx
@@ -15,18 +15,10 @@ function Nav() {
         quality={100}
       ></Image>
       <ul className='hidden lg:flex justify-between text-2xl w-1/3 cursor-pointer'>
-        <Link href={"/"}>
-          <h2>Home</h2>
-        </Link>
-        <Link href={"/"}>
-          <h2>Movies</h2>
-        </Link>
-        <Link href={"/"}>
-          <h2>TV Shows</h2>
-        </Link>
-        <Link href={"/"}>
-          <h2>On Air</h2>
-        </Link>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/"}>Movies</Link>
+        <Link href={"/"}>TV Shows</Link>
+        <Link href={"/"}>On Air</Link>
       </ul>
       <div className='text-2xl w-24 flex justify-between'>
         <button>
