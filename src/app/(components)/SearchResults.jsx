1d11750db8398747fad57c7df4661ccbc9397063
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-01-18 22:21:39 +0200

    style: add background image for search results and modify the alignment of the text

--- src/app/(components)/SearchResults.jsx
+++ src/app/(components)/SearchResults.jsx
@@ -14,34 +14,47 @@ async function SearchResults({ word }) {
       </h2>
       {results.length === 0 ? (
         <>
-          <h2>Nothing Found for "{word}"</h2>
+          <h2>
+            Nothing Found for <span className='text-c-blue'>"{word}"</span>{" "}
+          </h2>
           <h3>Try a different Search</h3>
         </>
       ) : (
-        <div className='grid grid-cols-2 gap-y-10'>
+        <div className='grid grid-cols-2 gap-y-10 '>
           {results.map((movie) => (
             <Link href={"/" + movie.id}>
-              <div key={movie.id} className=' grid grid-cols-2 mr-1 relative'>
-                <div>
+              <div key={movie.id} className='relative mr-1 w-[800] h-96 '>
+                <Image
+                  className='relative object-fit w-full h-full opacity-15'
+                  src={IMG_URL + movie.backdrop_path}
+                  width={200}
+                  height={500}
+                  quality={100}
+                  alt={movie.original_title}
+                />
+                <div className='absolute top-0 flex flex-col px-2 pt-4'>
                   <h2 className='text-xl mb-1 '>{movie.original_title}</h2>
-                  <Image
-                    className=' '
-                    src={IMG_URL + movie.poster_path}
-                    width={200}
-                    height={500}
-                    alt={movie.original_title}
-                  />
-                </div>
-                <div className='py-8'>
-                  <div className='flex justify-around w-1/2'>
-                    <h3 className='bg-c-blue py-0.5 px-2 rounded-md'>
-                      {movie.original_language.toUpperCase()}
-                    </h3>
-                    <h3 className=' bg-c-red p-2 rounded-full'>
-                      {movie.vote_average.toFixed(1)}
-                    </h3>
+                  <div className='flex'>
+                    <Image
+                      className='mr-2 '
+                      src={IMG_URL + movie.poster_path}
+                      width={200}
+                      height={500}
+                      alt={movie.original_title}
+                    />
+
+                    <div className='py-8'>
+                      <div className='flex justify-around w-1/2 mb-2'>
+                        <h3 className='bg-c-blue py-0.5 px-2 rounded-md'>
+                          {movie.original_language.toUpperCase()}
+                        </h3>
+                        <h3 className=' bg-c-red p-2 rounded-full'>
+                          {movie.vote_average.toFixed(1)}
+                        </h3>
+                      </div>
+                      <p>{movie.overview}</p>
+                    </div>
                   </div>
-                  <p>{movie.overview}</p>
                 </div>
               </div>
             </Link>
