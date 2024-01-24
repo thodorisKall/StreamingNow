acec16fdca344842821e1c474aa3b9fff89bbe76
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-01-25 00:35:52 +0200

    feat: display 4 times the same component with different props

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,15 @@ import PopularMovies from "./(components)/PopularMovies"
 
 export default async function Home() {
   return (
-    <section className='main__page'>
-      <h4>Popular Movies</h4>
-      <PopularMovies fetch_func={popularMovie} />
-      <h4>Popular TV Shows</h4>
-      <PopularMovies fetch_func={popularTv} />
-      <h4>Upcoming Movies</h4>
-      <PopularMovies fetch_func={upcoming} />
-    </section>
+    <>
+      <section className='main__page'>
+        <h4>Popular Movies</h4>
+        <PopularMovies fetch_func={popularMovie} />
+        <h4>Popular TV Shows</h4>
+        <PopularMovies fetch_func={popularTv} />
+        <h4>Upcoming Movies</h4>
+        <PopularMovies fetch_func={upcoming} />
+      </section>
+    </>
   )
 }
