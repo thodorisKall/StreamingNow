b7370ad2612a660389bb466c83d73e0881615c61
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-01-28 01:27:51 +0200

    feat: add url behind the Developed text and open it to a new tab

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,8 +25,16 @@ export default function RootLayout({ children }) {
 
         <Footer />
         <h3 className=' py-4 pl-4 lg:pl-20 font-semibold'>
-          Developed By <a className='text-c-blue'>Thodoris Kallioras </a>&#169;
-          2024
+          Developed By
+          <a
+            href='https://www.thodoriskallioras.com/'
+            target='_blank'
+            className='text-c-blue'
+          >
+            {" "}
+            Thodoris Kallioras{" "}
+          </a>
+          &#169; 2024
         </h3>
       </body>
     </html>
