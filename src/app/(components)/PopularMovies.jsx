8b882e9abe6edc75ab526c638b29f22f0bbe2c72
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-01-29 01:49:45 +0200

    feat: change image diamensions and grid gap width

--- src/app/(components)/PopularMovies.jsx
+++ src/app/(components)/PopularMovies.jsx
@@ -7,25 +7,27 @@ async function PopularMovies({ fetch_func }) {
 
   return (
     <section className='mb-8'>
-      <div className='grid grid-cols-2 gap-x-2 md:grid-cols-3 lg:grid-cols-6  '>
+      <div className='grid grid-cols-2 gap-y-6 gap-x-2 md:grid-cols-3 lg:grid-cols-6 lg:gap-x-20 lg:gap-y-10'>
         {movies.slice(0, 12).map((movie) => (
           <Link href={`/${movie.id}`}>
-            <div key={movie.id} className=' mb-8  mr-2 lg:w-60'>
+            <div key={movie.id} className='w-fit'>
               <div className='relative'>
                 <Image
-                  className=' rounded-md mb-2 object-fit w-full h-52  lg:h-80'
+                  className='rounded-md mb-2 h-52 w-auto md:w-auto md:h-72 lg:h-auto lg:w-full'
                   src={`${img_url}${movie.poster_path}`}
                   width='0'
                   height='0'
                   sizes='100vw'
                   alt={movie.title ? movie.title : movie.name}
                 />
-                <h3 className='absolute top-2 left-4 bg-c-red w-fit p-2 rounded-full'>
-                  {movie.vote_average.toFixed(1)}
-                </h3>
-                <h3 className='bg-c-blue absolute top-2 right-4 p-1 px-2  rounded-md'>
-                  {movie.original_language.toUpperCase()}
-                </h3>
+                <div>
+                  <h3 className='absolute top-2 left-4 bg-c-red w-fit p-2 rounded-full'>
+                    {movie.vote_average.toFixed(1)}
+                  </h3>
+                  <h3 className='absolute bg-c-blue bottom-2 left-4 p-1 px-2  rounded-md'>
+                    {movie.original_language.toUpperCase()}
+                  </h3>
+                </div>
               </div>
               <h2
                 id='suggestions_title'
