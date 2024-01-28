685eec951dfea3536510fb656cf00d007cfa34e1
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-01-29 01:47:24 +0200

    feat: create responsive drop down nav bar

--- src/app/(components)/Nav.jsx
+++ src/app/(components)/Nav.jsx
@@ -4,12 +4,14 @@ import Link from "next/link"
 import { useRouter } from "next/navigation"
 import Image from "next/image"
 import { IoIosSearch } from "react-icons/io"
-import { IoMoonOutlin, IoSunnyOutline, IoCloseSharp } from "react-icons/io5"
+import { IoMoonOutlin, IoSunnyOutline } from "react-icons/io5"
 import { FaBars } from "react-icons/fa6"
+import { CgClose } from "react-icons/cg"
 //
 
 function Nav() {
   const [searchText, setSearchText] = useState("")
+  const [showNav, setShowNav] = useState(false)
   const router = useRouter()
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -17,7 +19,7 @@ function Nav() {
   }
 
   return (
-    <nav className='flex flex-col lg:flex-row justify-between items-center w-full h-fit lg:h-24 mb-4 mt-4 lg:mt-0 '>
+    <nav className='flex flex-col relative lg:flex-row justify-between items-center w-full h-fit lg:h-24 mb-4 mt-4 lg:mt-0 '>
       <Image
         src='/nowStreaming_icon.png'
         width={200}
@@ -26,6 +28,20 @@ function Nav() {
         quality={100}
         className='mb-4'
       ></Image>
+      <button
+        onClick={() => setShowNav(!showNav)}
+        className='lg:hidden absolute right-0 text-xl text-c-white'
+      >
+        {showNav ? <CgClose /> : <FaBars />}
+      </button>
+      {showNav && (
+        <div className='nav__sm lg:hidden bg-light_black flex flex-col w-full mb-4 pt-2 px-2'>
+          <Link href={"/"}>Home</Link>
+          <Link href={"/topRated"}>Top Rated</Link>
+          <Link href={"/onAir"}>On Air</Link>
+          <Link href={"/upcoming"}>Upcoming</Link>
+        </div>
+      )}
       <ul className='hidden lg:flex justify-between text-2xl w-1/3 cursor-pointer font-semibold'>
         <Link href={"/"}>Home</Link>
         <Link href={"/topRated"}>Top Rated</Link>
